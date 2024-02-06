f792f6de89906c9f5970d132005826af012b0d65
Rashad Clement <114028394+RashadC30@users.noreply.github.com>
2024-02-06 08:49:40 -0800

    modified nex.config

--- next.config.js
+++ next.config.js
@@ -1,5 +1,9 @@
 /** @type {import('next').NextConfig} */
+
+const isProd = (process.env.NODE_ENV = "production");
+
 const nextConfig = {
+  basePath: isProd ? "/Overflo_Webpage" : "",
   output: "export",
   distDir: "dist",
   images: {
